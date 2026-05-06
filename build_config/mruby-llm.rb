442b981ba7388dd249cb053919508ae9019e085f
0x1eef <0x1eef@hardenedbsd.org>
2026-05-05 21:56:08 -0300

    feat(build): add build_config/

---
+++ build_config/mruby-llm.rb
@@ -0,0 +1,14 @@
+MRuby::Build.new("mruby-llm") do |conf|
+  conf.toolchain
+  conf.gembox "default"
+  conf.enable_debug
+
+  # FreeBSD and similar systems install libcurl under /usr/local.
+  conf.cc.include_paths << "/usr/local/include"
+  conf.linker.library_paths << "/usr/local/lib"
+
+  conf.gem File.expand_path("..", __dir__)
+
+  conf.enable_bintest
+  conf.enable_test
+end

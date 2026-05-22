709a632873440e5d04c0df7675a4fa249b2b430b
0x1eef <0x1eef@hardenedbsd.org>
2026-05-22 00:22:13 -0300

    uri: workaround mruby bug

--- mrblib/mruby-llm/uri/parsed.rb
+++ mrblib/mruby-llm/uri/parsed.rb
@@ -8,7 +8,91 @@ module LLM::URI
   # It only implements the subset of URI state currently needed by the
   # transport layer.
   class Parsed
-    PATTERN = /\A([a-z][a-z0-9+\-.]*):\/\/([^\/?#:]+)(?::(\d+))?([^?#]*)?(\?[^#]*)?/
+    ##
+    # Separate regexp are required due to https://github.com/mruby/mruby/issues/6853
+    ABSOLUTE_HOST_PORT_PATH_QUERY_PATTERN = %r{
+      \A
+      (?<scheme>[a-z][a-z0-9+\-.]*)
+      ://
+      (?<host>[A-Za-z0-9.\-]+)
+      :
+      (?<port>\d+)
+      (?<path>/[^\?#]*)
+      (?<query>\?[^\#]*)?
+      (?:\#.*)?
+      \z
+    }x
+    ABSOLUTE_HOST_PORT_QUERY_PATTERN = %r{
+      \A
+      (?<scheme>[a-z][a-z0-9+\-.]*)
+      ://
+      (?<host>[A-Za-z0-9.\-]+)
+      :
+      (?<port>\d+)
+      (?<query>\?[^\#]*)
+      (?:\#.*)?
+      \z
+    }x
+    ABSOLUTE_HOST_PATH_QUERY_PATTERN = %r{
+      \A
+      (?<scheme>[a-z][a-z0-9+\-.]*)
+      ://
+      (?<host>[A-Za-z0-9.\-]+)
+      (?<path>/[^\?#]*)
+      (?<query>\?[^\#]*)?
+      (?:\#.*)?
+      \z
+    }x
+    ABSOLUTE_HOST_PATH_PATTERN = %r{
+      \A
+      (?<scheme>[a-z][a-z0-9+\-.]*)
+      ://
+      (?<host>[A-Za-z0-9.\-]+)
+      (?<path>/[^\?#]*)
+      \z
+    }x
+    ABSOLUTE_HOST_QUERY_PATTERN = %r{
+      \A
+      (?<scheme>[a-z][a-z0-9+\-.]*)
+      ://
+      (?<host>[A-Za-z0-9.\-]+)
+      (?<query>\?[^\#]*)
+      (?:\#.*)?
+      \z
+    }x
+    ABSOLUTE_HOST_PORT_PATTERN = %r{
+      \A
+      (?<scheme>[a-z][a-z0-9+\-.]*)
+      ://
+      (?<host>[A-Za-z0-9.\-]+)
+      :
+      (?<port>\d+)
+      \z
+    }x
+    ABSOLUTE_HOST_PORT_PATH_PATTERN = %r{
+      \A
+      (?<scheme>[a-z][a-z0-9+\-.]*)
+      ://
+      (?<host>[A-Za-z0-9.\-]+)
+      :
+      (?<port>\d+)
+      (?<path>/[^\?#]*)
+      (?:\#.*)?
+      \z
+    }x
+    ABSOLUTE_HOST_PATTERN = %r{
+      \A
+      (?<scheme>[a-z][a-z0-9+\-.]*)
+      ://
+      (?<host>[A-Za-z0-9.\-]+)
+      \z
+    }x
+    RELATIVE_PATTERN = %r{
+      \A
+      (?<path>[^\#]*)
+      (?:\#.*)?
+      \z
+    }x
 
     ##
     # @return [String, nil]
@@ -31,19 +115,52 @@ module LLM::URI
     # @return [LLM::URI::Parsed]
     def initialize(value)
       @value = value.to_s
-      match = PATTERN.match(@value)
-      if match
-        @scheme = match[1]
-        @host = match[2]
-        @port = match[3] ? match[3].to_i : default_port(@scheme)
-        path = match[4].to_s
-        path = "/" if path.empty?
-        @request_uri = "#{path}#{match[5]}"
+      if match = ABSOLUTE_HOST_PORT_PATH_QUERY_PATTERN.match(@value)
+        @scheme = match[:scheme]
+        @host = match[:host]
+        @port = match[:port].to_i
+        @request_uri = "#{match[:path].to_s.empty? ? "/" : match[:path]}#{match[:query]}"
+      elsif match = ABSOLUTE_HOST_PORT_QUERY_PATTERN.match(@value)
+        @scheme = match[:scheme]
+        @host = match[:host]
+        @port = match[:port].to_i
+        @request_uri = "/#{match[:query]}"
+      elsif match = ABSOLUTE_HOST_PATH_QUERY_PATTERN.match(@value)
+        @scheme = match[:scheme]
+        @host = match[:host]
+        @port = default_port(@scheme)
+        @request_uri = "#{match[:path].to_s.empty? ? "/" : match[:path]}#{match[:query]}"
+      elsif match = ABSOLUTE_HOST_PATH_PATTERN.match(@value)
+        @scheme = match[:scheme]
+        @host = match[:host]
+        @port = default_port(@scheme)
+        @request_uri = match[:path]
+      elsif match = ABSOLUTE_HOST_QUERY_PATTERN.match(@value)
+        @scheme = match[:scheme]
+        @host = match[:host]
+        @port = default_port(@scheme)
+        @request_uri = "/#{match[:query]}"
+      elsif match = ABSOLUTE_HOST_PORT_PATTERN.match(@value)
+        @scheme = match[:scheme]
+        @host = match[:host]
+        @port = match[:port].to_i
+        @request_uri = "/"
+      elsif match = ABSOLUTE_HOST_PORT_PATH_PATTERN.match(@value)
+        @scheme = match[:scheme]
+        @host = match[:host]
+        @port = match[:port].to_i
+        @request_uri = match[:path]
+      elsif match = ABSOLUTE_HOST_PATTERN.match(@value)
+        @scheme = match[:scheme]
+        @host = match[:host]
+        @port = default_port(@scheme)
+        @request_uri = "/"
       else
+        match = RELATIVE_PATTERN.match(@value)
         @scheme = nil
         @host = nil
         @port = nil
-        @request_uri = @value.empty? ? "/" : @value
+        @request_uri = match[:path].to_s.empty? ? "/" : match[:path]
       end
     end
 
